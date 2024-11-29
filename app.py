65dde852bd272788c43d9e92d56849aacb9dd9a4
sakichika <saki@DESKTOP-5P6EK8K.localdomain>
2024-11-29 19:06:05 +0900

    Initial Release

--- app.py
+++ app.py
@@ -34,6 +34,40 @@ app.config.update({
 })
 
 app.secret_key = os.getenv("FLASK_SECRET_KEY", "default-secret-key")
+
+from flask_session.sessions import RedisSessionInterface
+from itsdangerous import want_bytes
+
+# セッション保存処理のパッチ
+class PatchedRedisSessionInterface(RedisSessionInterface):
+    def save_session(self, app, session, response):
+        if not session:
+            self._delete_session(app, session)
+            return
+
+        # セッションIDを文字列に変換
+        session_id = self._get_signer(app).sign(want_bytes(session.sid)).decode("utf-8")
+
+        response.set_cookie(
+            app.session_cookie_name,
+            session_id,
+            max_age=self.get_expiration_time(app, session),
+            expires=self.get_expiration_time(app, session),
+            path=self.get_cookie_path(app),
+            domain=self.get_cookie_domain(app),
+            secure=self.get_cookie_secure(app),
+            httponly=self.get_cookie_httponly(app),
+            samesite=self.get_cookie_samesite(app),
+        )
+        self.redis.setex(
+            self.key_prefix + session_id,
+            int(self.permanent_session_lifetime.total_seconds()),
+            self.serializer.dumps(dict(session)),
+        )
+
+# パッチしたセッションインターフェースを適用
+app.session_interface = PatchedRedisSessionInterface(redis_client, app.config["SESSION_KEY_PREFIX"])
+
 # Flask-Session の初期化
 Session(app)
 
